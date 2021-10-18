13d657024600484c127ff93fa45697885c68b732
asus <asus@DESKTOP-7AA3IH2>
2021-10-19 00:03:27 +0300

    18 ekim

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("dilek eda ")
+
+var dolarDun =9.20
+var dolarBugun = 10
+
+
+dolarDun="jsnak"
+console.log(dolarDun)
+
+let konuk=["eda","dilek","kamu"]
+console.log("<ul>")
+console.log("</ul>")
+for (let i=0; i<3;i++){
+    console.log("<li>"+i+"</li>")
+}
+console.log("</ul>")
+{/* <ul>
+    <li>eda</li>
+    <li>dilek</li>
+    <li>kamu</li>
+</ul> */}
